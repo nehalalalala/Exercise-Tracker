b4f076b62d2c02e5181c287c51c6cb1d807ebd83
nehalalalala <118789876+nehalalalala@users.noreply.github.com>
2024-10-10 15:40:36 +0530

    Update LandingPage.component.js

--- src/components/LandingPage.component.js
+++ src/components/LandingPage.component.js
@@ -12,8 +12,8 @@ const LandingPage = () => {
   return (
     <div className="landing-page">
       <div className="hero-section">
-        <h1>Welcome to Excercise Tracker</h1>
-        <p>Your journey to awesomeness starts here.</p>
+        <h1>Welcome to Synapse</h1>
+        <p>Stay motivated and track your fitness journey with ease. Synapse offers a personalized fitness log to monitor your progress, set goals, and stay accountable. Connect with a supportive community of like-minded individuals who share their daily achievements and offer encouragement. Let's make your fitness goals a reality together. </p>
         <button className="get-started-button" onClick={handleGetStarted}>
           Get Started
         </button>
